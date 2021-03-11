f244adbfac8ea9277dc1ffc1af8282f0814a3c9a
Raoul-S <20110219@student.hhs.nl>
2021-03-11 16:01:30 +0100

    Menu/Main toegevoegd
    Een menu aangemaakt die een System.out.println terug geeft. Een main toegevoegd om daar alles aan te maken en aan te roepen.

---
+++ Main.java
@@ -0,0 +1,12 @@
+class Main{
+    
+    public static void main(String[] args){
+        //Alle data inlezen
+
+        Menu menu = new Menu();
+        menu.getMenu();
+        menu.getKeuze();
+
+ 
+    }
+}
---
+++ Menu.java
@@ -0,0 +1,52 @@
+import java.util.Scanner;
+
+class Menu {
+    
+    public void getMenu(){
+        System.out.println("1) Lijst met examens");
+        System.out.println("2) Lijst met studenten");
+        System.out.println("3) Nieuwe student inschrijven");
+        System.out.println("4) Student verwijderen");
+        System.out.println("5) Examen afnemen");
+        System.out.println("6) Is student geslaagd voor test?");
+        System.out.println("7) Welke examens heeft student gehaald");
+        System.out.println("8) Welke student heeft de meeste examens gehaald?");
+        System.out.println("0) exit");
+        System.out.print("Uw keuze:");
+    }
+
+    public void getKeuze(){
+        Scanner scanner = new Scanner(System.in);
+        Integer keuze = scanner.nextInt();
+
+        while(keuze > 8){
+            System.out.print("Maak uw keuze:");
+            keuze = scanner.nextInt();
+          }
+          System.out.println();
+          switch (keuze) {
+            case 1:
+                System.out.println("Monday");
+              break;
+            case 2:
+                System.out.println("Tuesday");
+              break;
+            case 3:
+                System.out.println("Wednesday");
+              break;
+            case 4:
+                System.out.println("Thursday");
+              break;
+            case 5:
+              System.out.println("Friday");
+              break;
+            case 6:
+              System.out.println("Saturday");
+              break;
+            case 7:
+              System.out.println("Sunday");
+              break;
+        }
+        scanner.close();
+    }
+}
