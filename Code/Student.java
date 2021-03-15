8b824ff81b64749b6da64d70531b55dc055baff2
BilalElhassani <80320741+BilalElhassani@users.noreply.github.com>
2021-03-15 13:17:56 +0100

    Update Student.java
    tekst toegevoegd om overzichtelijker te maken

--- Code/Student.java
+++ Code/Student.java
@@ -1,35 +1,39 @@
 import java.util.ArrayList;
 
 public class Student {
+    //Instance Variables
     private final Integer studentcode;
     private final String naam;
     private ArrayList <Student> Studenten = new ArrayList<Student>();
     private Integer exBehaald = 0;
     private ArrayList <Examen> maakt;// moet dit niet weg, want er is al een getExamens?
 
+    //Constructor
     public Student(Integer studentcode, String naam)
-        {
-            this.studentcode = studentcode;
-            this.naam = naam;
-            Studenten.add(this);
-        }
-public String getNaam()
-{
-    return this.naam;
-}
-
-public void addStudent(Student Student) //Volgens mij moeten we hier Student Student meegeven maar staat niet in diagram
-{
-    Studenten.add(Student);
-}
-
-public ArrayList<Student> showStudenten()
-{
-    return Studenten;
-}
-
-public void deleteStudenten(Student Student)
-{
-    Studenten.remove(Student);
-}
+    {
+        this.studentcode = studentcode;
+        this.naam = naam;
+        Studenten.add(this);
     }
+    
+    //Instance Methods
+    public String getNaam()
+    {
+        return this.naam;
+    }
+
+    public void addStudent(Student Student) //Volgens mij moeten we hier Student Student meegeven maar staat niet in diagram
+    {
+        Studenten.add(Student);
+    }
+
+    public ArrayList<Student> showStudenten()
+    {
+        return Studenten;
+    }
+
+    public void deleteStudenten(Student Student)
+    {
+        Studenten.remove(Student);
+    }
+}
