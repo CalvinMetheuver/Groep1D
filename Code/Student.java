20245631dc019fe934793199f7efd36f31341d10
SufyaanGit <80320867+SufyaanGit@users.noreply.github.com>
2021-03-15 11:14:47 +0100

    Add files via upload
    Niet af

---
+++ Code/Student.java
@@ -0,0 +1,35 @@
+import java.util.ArrayList;
+
+public class Student {
+    private final Integer studentcode;
+    private final String naam;
+    private ArrayList <Student> Studenten = new ArrayList<Student>();
+    private Integer exBehaald;
+    private ArrayList <Examen> maakt;
+
+    public Student(Integer studentcode, String naam)
+        {
+            this.studentcode = studentcode;
+            this.naam = naam;
+            Studenten.add(this);
+        }
+public String getNaam()
+{
+    return naam;
+}
+
+public void addStudent(Student Student) //Volgens mij moeten we hier Student Student meegeven maar staat niet in diagram
+{
+    Studenten.add(Student);
+}
+
+public ArrayList<Student> showStudenten()
+{
+    return Studenten;
+}
+
+public void deleteStudenten()
+{
+//?
+}
+    }
