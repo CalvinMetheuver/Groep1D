e6e832f15bd4ca10c9e18188bce2b12567edaa3c
Raoul-S <20110219@student.hhs.nl>
2021-03-23 12:57:40 +0100

    ReadFiles opgeschoond
    Functies weggehaald die niet meer nodig waren

--- Code/ReadFiles.java
+++ Code/ReadFiles.java
@@ -1,6 +1,5 @@
 import java.io.File;
 import java.io.FileNotFoundException;
-import java.util.ArrayList;
 import java.util.Scanner;
 
 //functies voor het uitlezen van de opgeslagen data
@@ -19,10 +18,7 @@ class ReadFiles {
         }
     }
 
-
-
     //test fabian voor antwoorden en vragen in 1 .txt uitlezen
-
   public static void readJavaQandA() throws FileNotFoundException{
         try {
             File file = new File("javaAnswersAndQuestions.txt");
@@ -46,74 +42,6 @@ class ReadFiles {
         }
     }
 
-    // leest 2 aparte bestanden in
-    public static void readJava() throws FileNotFoundException{
-
-        File file = new File("javaQuestions.txt");
-        String f = file.getAbsolutePath();
-        Scanner scanner = new Scanner(new File(f));
-
-        File file1 = new File("javaAnswers.txt");
-        String f1 = file1.getAbsolutePath();
-        Scanner scanner1 = new Scanner(new File(f1));
-
-        while(scanner.hasNextLine()){
-            String javaQuestions = scanner.nextLine();
-            String javaAnswers = scanner1.nextLine();
-
-            // Exam java = new Exam("java");
-            new Assignment(javaQuestions, javaAnswers);
-            // java.printAssignments();
-        }
-    }
-
-    /*
-    leest alleen javavragen in,zodat ik het in de main als parameter kon meegeven,
-
-    werkt niet want elke keer als het object word aangeroepen print het dezelfde line.
-    ik had het zo in de main gezet:
-    java = var van Exam
-    java.addAssignment(ReadFiles.readJavaQuestions(), ReadFiles.readJavaAnswers());
-    */
-
-    public static String readJavaQuestions() throws FileNotFoundException{
-        try {
-            File file = new File("javaQuestions.txt");
-            String f = file.getAbsolutePath();
-            Scanner scanner = new Scanner(new File(f));
-
-            while(scanner.hasNextLine()){
-                System.out.println(scanner.nextLine());
-                return scanner.nextLine();
-            }
-
-        } catch (FileNotFoundException e) {
-            e.printStackTrace();
-        }
-        return null;
-    }
-
-    // zelfde als hierboven
-    public static String readJavaAnswers() throws FileNotFoundException{
-        try {
-        File file = new File("javaAnswers.txt");
-        String f = file.getAbsolutePath();
-        Scanner scanner = new Scanner(new File(f));
-
-            while(scanner.hasNextLine()){
-                System.out.println(scanner.nextLine());
-                return scanner.nextLine();
-            }
-
-            } catch (FileNotFoundException e) {
-                e.printStackTrace();
-        }
-        return null;
-    }
-
-
-
-
     // public static void readStudenten() throws FileNotFoundException{
     //     Scanner scanner = new Scanner(new File("Studenten.txt"));
     //     int nummer;
@@ -126,9 +54,4 @@ class ReadFiles {
     //         new Student(nummer,naam);
     //     }
     // }
-
-
-
-
-
 }
