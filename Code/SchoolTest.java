26c48605cf6ea72a9edc1b8ccdbbab0dea07341c
BilalElhassani <dakar_ali9@hotmail.com>
2021-03-25 11:12:31 +0100

    Create SchoolTest.java
    SchoolTest aangemaakt

---
+++ Code/SchoolTest.java
@@ -0,0 +1,34 @@
+import org.junit.jupiter.api.Test;
+
+import static org.junit.jupiter.api.Assertions.*;
+
+class SchoolTest {
+
+    @Test
+    void showStudents() {
+        Student student = new Student(20195745, "Bilal");
+        assertEquals(20195745, student.getStudentCode());
+        assertEquals("Bilal", student.getName());
+    }
+
+    @Test
+    void addToStudentList() {
+
+    }
+
+    @Test
+    void getStudent() {
+    }
+
+    @Test
+    void showExams() {
+    }
+
+    @Test
+    void addStudent() {
+    }
+
+    @Test
+    void deleteStudent() {
+    }
+}
