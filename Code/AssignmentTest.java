5c44e0666364d08fab6274219868fff372f16e41
fabrree <80321003+fabrree@users.noreply.github.com>
2021-03-26 15:25:21 +0100

    AssignmentTest begin gemaakt.

---
+++ Code/AssignmentTest.java
@@ -0,0 +1,65 @@
+import org.junit.jupiter.api.Test;
+
+import java.util.Scanner;
+
+import static org.junit.jupiter.api.Assertions.*;
+
+class AssignmentTest {
+
+    Assignment assignment = new Assignment("Vraag?", "Antwoord");
+
+    //getQuestions
+    @Test
+    void getQuestion() {
+
+        //Positief
+        assertEquals("Vraag?", assignment.getQuestion());
+        assertEquals("Antwoord", assignment.getAnswer());
+
+    }
+
+    @Test
+    void getQuestionShouldReturnFalse() {
+
+        //Negetief
+        assertEquals("Vrag?", assignment.getQuestion());
+        assertEquals("Antword", assignment.getAnswer());
+    }
+
+    //getAnswer
+    @Test
+    void getAnswer() {
+
+    }
+
+    @Test
+    void getAnswerShouldReturnFalse() {
+
+    }
+
+    //takeAssignment
+    @Test
+    void takeAssignment() {
+        /*
+        Scanner scanner = new Scanner(System.in);
+        System.out.println("Typ");
+        String input = scanner.nextLine();
+        System.out.println("input");
+
+        String goedAnt = "a";
+        assertTrue(input.equalsIgnoreCase(goedAnt));
+         */
+
+        String input = "A";
+        String goedAnt = "a";
+        assertTrue(input.equalsIgnoreCase(goedAnt));
+    }
+
+    @Test
+    void takeAssignmentShouldReturnFalse() {
+        String input = "b";
+        String goedAnt = "a";
+        assertTrue(input.equalsIgnoreCase(goedAnt));
+
+    }
+}
---
+++ Code/ExamTest.java
@@ -0,0 +1,29 @@
+import org.junit.jupiter.api.Test;
+
+import java.util.ArrayList;
+
+import static org.junit.jupiter.api.Assertions.*;
+
+class ExamTest {
+
+    @Test
+    void getAssignment() {
+    }
+
+    @Test
+    void getExams() {
+    }
+
+    @Test
+    void getName() {
+    }
+
+    @Test
+    void addAssignment() {
+
+    }
+
+    @Test
+    void takeExam() {
+    }
+}
