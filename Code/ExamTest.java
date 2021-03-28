8edadb38a69a41c7ae624b554146e98434039e18
fabrree <80321003+fabrree@users.noreply.github.com>
2021-03-28 21:13:21 +0200

    addAssignment werkt nog niet en takeExam moeten we nog maken

--- Code/ExamTest.java
+++ Code/ExamTest.java
@@ -1,3 +1,4 @@
+import org.junit.jupiter.api.BeforeAll;
 import org.junit.jupiter.api.Test;
 
 import java.util.ArrayList;
@@ -6,24 +7,46 @@ import static org.junit.jupiter.api.Assertions.*;
 
 class ExamTest {
 
+    private static Assignment assignment;
+    private static Exam exams;
+
+
+
+
+    @BeforeAll
+    public static void initEach(){
+        exams = new Exam("Java");
+        assignment = new Assignment("q?", "a");
+        System.out.println("Before alll is called");
+    }
+
     @Test
     void getAssignment() {
+
+        Assignment a = new Assignment("q?", "a");
+        assertEquals(a.getQuestion(), assignment.getQuestion());
+
     }
 
     @Test
     void getExams() {
+        assertEquals("Java", exams.getName());
     }
 
     @Test
     void getName() {
+        String naam = "AK";
+        assertEquals("AK",naam);
     }
 
     @Test
     void addAssignment() {
 
+        exams.addAssignment("Q?", "A");
+        assertEquals(assignment, exams.getAssignment()); //DOET HET NIET
     }
 
     @Test
-    void takeExam() {
+    void takeExam(){
     }
 }
