14136299d22a6c22cecb7ca2f43075df0788a6d7
fabrree <80321003+fabrree@users.noreply.github.com>
2021-03-29 16:36:22 +0200

    laatste aanpassingen

--- Code/AssignmentTest.java
+++ Code/AssignmentTest.java
@@ -11,36 +11,24 @@ class AssignmentTest {
 
     private static Assignment assignment;
 
-
-    @BeforeAll
-    public static void initEach() {
-        System.out.println("Dit wordt 1 keer uitgevoerd voordat de tests beginnen");
-    }
-
-
     @Test
     void takeAssignmentCorrectInput() {
         String answer = "A".toLowerCase();
-        assertEquals("a", answer);
-        System.out.println(answer);
+        assertEquals("a", answer); //test to lowercase
 
         String goedAnt = "a";
-        System.out.println(goedAnt);
 
-        // is gelijk, zou niet in de while moeten komen invoer,
-        // antwoord gelijk (A / a)
         assertTrue(goedAnt.equals(answer));
     }
 
     @Test
     void takeAssignmentWrongInput() {
         String answer = "B".toLowerCase();
-        System.out.println(answer);
-        Scanner testScanner = new Scanner(answer);
+        assertEquals("b", answer); //test to lowercase
+
         String goedAnt = "a";
-        // is ongelijk, zou WEL in de while moeten komen en nieuwe invoer opvragen,
-        //antwoord ongelijk (B / a)
-        assertTrue(!(goedAnt.equals(testScanner)));
+
+        assertTrue((goedAnt.equals(goedAnt)));
     }
 
 }
--- Code/ExamTest.java
+++ Code/ExamTest.java
@@ -7,8 +7,10 @@ import static org.junit.jupiter.api.Assertions.*;
 
 class ExamTest {
 
-    private static Assignment assignment;
+    //private static Assignment assignment;
     private static Exam exams;
+    private static ArrayList <Assignment>  assignments = new ArrayList<>();
+
 
 
 
@@ -16,8 +18,40 @@ class ExamTest {
     @BeforeAll
     public static void initEach(){
         exams = new Exam("Java");
-        assignment = new Assignment("q?", "a");
+
         System.out.println("Before alll is called");
+    }
+
+    @Test
+    void addAssignment(){
+
+        assignments.add(new Assignment("V", "A"));
+        assignments.add(new Assignment("Vr", "An"));
+        assignments.add(new Assignment("Vra", "Ant"));
+        assignments.add(new Assignment("Vraa", "Antwo"));
+        assignments.add(new Assignment("Vraag", "Antwoord"));
+        assignments.add(new Assignment("Vraag 6", "Antwoord 6"));
+        assignments.add(new Assignment("Vraag 7", "Antwoord 7"));
+        assignments.add(new Assignment("Vraag 8", "Antwoord 8"));
+        assignments.add(new Assignment("Vraag 9", "Antwoord 9"));
+
+
+        assertEquals("V",assignments.get(0).getQuestion());
+        assertEquals("Vr",assignments.get(1).getQuestion());
+        assertEquals("Vra",assignments.get(2).getQuestion());
+        assertEquals("Vraa",assignments.get(3).getQuestion());
+        assertEquals("Vraag",assignments.get(4).getQuestion());
+
+        assertEquals("A",assignments.get(0).getAnswer());
+        assertEquals("An",assignments.get(1).getAnswer());
+        assertEquals("Ant",assignments.get(2).getAnswer());
+        assertEquals("Antwo",assignments.get(3).getAnswer());
+        assertEquals("Antwoord",assignments.get(4).getAnswer());
+
+    }
+
+    @Test
+    void takeExam() {
     }
 
 
